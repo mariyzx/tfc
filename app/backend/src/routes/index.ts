647fc689cf1620771f7fe744e062842f964b5d82
Mariana Werneck <marinhomariana8@gmail.com>
2022-12-04 16:12:02 -0300

    :feat: add swagger

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -3,9 +3,12 @@ import Login from './Login.routes';
 import Teams from './Team.routes';
 import Matches from './Match.routes';
 import Leaderboard from './Leaderboard.routes';
+import * as swaggerDoc from '../swagger.json';
+import * as swaggerUi from 'swagger-ui-express'
 
 const router = Router();
 
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc) )
 router.use('/login', Login);
 router.use('/teams', Teams);
 router.use('/matches', Matches);
