da09c48f4bf65345cc8fcf8f30ef3363447bdccd
Mariana Werneck <marinhomariana8@gmail.com>
2023-02-10 17:12:53 -0300

    :boom: :fix: add correct ambient variables on database.ts

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,13 +2,16 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.PGHOST ,
-  port: Number(process.env.PGPORT),
-  dialect: 'postgres',
-  logging: true,
+  host: process.env.DB_HOST ,
+  port: Number(process.env.DB_PORT),
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
 }
 
 module.exports = config;
